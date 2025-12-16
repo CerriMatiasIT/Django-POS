5c1c55e16b7f42543eb52a7e38d6c69f74f2a04d
Matias Cerri <cerrimatiasit@gmail.com>
2025-12-16 09:46:17 -0300

    Ajuste purchases/Admin.py registro automatico

--- GastroPCN/purchases/admin.py
+++ GastroPCN/purchases/admin.py
@@ -10,3 +10,8 @@ class CompraEntradaAdmin(admin.ModelAdmin):
     list_display = ("id", "fecha", "proveedor", "total")
     search_fields = ("proveedor__nombre",)
     inlines = [DetalleEntradaInline]
+    
+    def save_model(self, request, obj, form, change):
+        if not obj.creada_por:
+            obj.creada_por = request.user
+        super().save_model(request, obj, form, change)
